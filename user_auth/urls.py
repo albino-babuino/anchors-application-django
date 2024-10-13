7500757fa50309f44f2759de23082f50b37e6309
Sergio Garcia Lopez <sergio.gl@outlook.com>
2024-10-13 13:16:40 +0200

    evitado que cuando el usuario está logueado pueda ir a la página de registro o login

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import RegisterView, HomePage
-
+from .views import RegisterView, HomePage,CustomLoginView
 
 urlpatterns = [
-    path('', HomePage.as_view(), name='home'),  # Página de inicio
-    path('login/', auth_views.LoginView.as_view(template_name='user_auth/login.html'), name='login'),
+    path('', HomePage.as_view(), name='home'),
+    path('login/', CustomLoginView.as_view(), name='login'), 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
-
-
-
--- user_auth/views.py
+++ user_auth/views.py
@@ -2,15 +2,28 @@ from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView, View
+from django.contrib.auth import views as auth_views
 
 class HomePage(View):
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('index')  # Redirige a /categories si el usuario está autenticado
+            return redirect('categories')  # Redirige a /categories si el usuario está autenticado
         return render(request, 'user_auth/home.html')
     
+class CustomLoginView(auth_views.LoginView):
+    template_name = 'user_auth/login.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('categories')  # Redirigir a /categories si ya está autenticado
+        return super().dispatch(request, *args, **kwargs)
+    
 class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = 'user_auth/register.html'
     success_url = reverse_lazy('login')
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('categories')  # Redirigir a /categories si ya está autenticado
+        return super().dispatch(request, *args, **kwargs)
